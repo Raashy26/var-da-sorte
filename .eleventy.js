a46e2133a94637ab3ef12f191ab07646475c1c9d
Renato <renatomlima26@gmail.com>
2025-09-01 15:35:04 +0100

    atualização geral VAR da sorte

--- .eleventy.js
+++ .eleventy.js
@@ -25,7 +25,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      includes: "_includes",
+      includes: "includes",
       data: "data",
       output: "_site",
     },
